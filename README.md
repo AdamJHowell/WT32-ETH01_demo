d6d442af7f46aa055643491195d2a4be8e083195
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-03-07 08:43:58 -0700

    Updated README.md and file formatting changes.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WT32-ETH01_demo
-Sketch to figure out how to use this board.
+This is a project that I will use to experiment with this board.
+Since this is my first ESP32 with an Ethernet port, much of my usual code will need to be modified.
+I plan on modifying my MQTT connection method to select a broker, from an array of brokers, based which is pingable.
--- WT32-ETH01_demo.ino
+++ WT32-ETH01_demo.ino
@@ -2,22 +2,22 @@
 #define TX_LED 17
 
 
-void setup() 
+void setup()
 {
-  pinMode( RX_LED, OUTPUT );
-  pinMode( TX_LED, OUTPUT );
-  Serial.begin( 115200 );
+	pinMode( RX_LED, OUTPUT );
+	pinMode( TX_LED, OUTPUT );
+	Serial.begin( 115200 );
 }
 
 
-void loop() 
+void loop()
 {
-  digitalWrite( RX_LED, HIGH );
-  digitalWrite( TX_LED, LOW );
-  Serial.println( "LED on" );
-  delay( 1000 );
-  digitalWrite( RX_LED, LOW );
-  digitalWrite( TX_LED, HIGH );
-  Serial.println( "LED off");
-  delay( 1000 );
+	digitalWrite( RX_LED, HIGH );
+	digitalWrite( TX_LED, LOW );
+	Serial.println( "LED on" );
+	delay( 1000 );
+	digitalWrite( RX_LED, LOW );
+	digitalWrite( TX_LED, HIGH );
+	Serial.println( "LED off" );
+	delay( 1000 );
 }
