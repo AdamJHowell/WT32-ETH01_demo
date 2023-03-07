cb7b3f04fe4ddaf4dabeecc68e7e426e094697b4
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-03-07 00:29:22 -0700

    Initial commit

---
+++ WT32-ETH01_demo.ino
@@ -0,0 +1,23 @@
+#define RX_LED 5
+#define TX_LED 17
+
+
+void setup() 
+{
+  pinMode( RX_LED, OUTPUT );
+  pinMode( TX_LED, OUTPUT );
+  Serial.begin( 115200 );
+}
+
+
+void loop() 
+{
+  digitalWrite( RX_LED, HIGH );
+  digitalWrite( TX_LED, LOW );
+  Serial.println( "LED on" );
+  delay( 1000 );
+  digitalWrite( RX_LED, LOW );
+  digitalWrite( TX_LED, HIGH );
+  Serial.println( "LED off");
+  delay( 1000 );
+}
